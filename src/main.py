c49d893b3da3f25987e41e136c6c964b72c2ddae
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-07-17 21:18:57 -0300

    feat: Update main.py and improves the lifecycle

--- src/main.py
+++ src/main.py
@@ -1,30 +1,48 @@
 import asyncio
 import uvicorn
+from contextlib import asynccontextmanager
+from typing import Dict
+
+from fastapi import FastAPI, Depends
+from redis.asyncio import Redis
+from fastapi_mail import FastMail
 
 from config import settings
-from contextlib import asynccontextmanager
-from database import init_redis_pool, close_redis_pool, get_redis_client
-from fastapi import FastAPI
-
+from redis_client import init_redis_pool, close_redis_pool, get_redis_client
 from notification_service.consumer import RabbitMQConsumer
 from notification_service.router import router as notification_router
+from notification_service.service import EmailService, EventHandler, get_mail_config
+
+app_state: Dict = {}
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_redis_pool()
-    redis_conn = await get_redis_client()
-    await redis_conn.ping()
-    print("Conexão com Redis verificada com sucesso.")
+    redis_client = await get_redis_client()
 
-    consumer = RabbitMQConsumer(
-        redis_client=redis_conn,
-    )
+    mail_config = get_mail_config(settings)
+    email_service = EmailService(FastMail(mail_config))
+    event_handler = EventHandler(redis_client=redis_client, email_service=email_service)
+    
+    consumer = RabbitMQConsumer(event_handler=event_handler)
+    
     consumer_task = asyncio.create_task(consumer.run())
-    print("Tarefa do consumidor iniciada em segundo plano.")
+    app_state["consumer_task"] = consumer_task
+    
+    print("Startup concluído. A aplicação está pronta para receber requisições.")
+    
     yield
-    print("Cancelando a tarefa do consumidor...")
-    consumer_task.cancel()
-    await consumer_task
+    
+    print("Executando tarefas de shutdown...")
+    
+    consumer_task = app_state.get("consumer_task")
+    if consumer_task:
+        consumer_task.cancel()
+        try:
+            await consumer_task
+        except asyncio.CancelledError:
+            print("Tarefa do consumidor cancelada com sucesso.")
+            
     await close_redis_pool()
 
 def create_app() -> FastAPI:
@@ -36,14 +54,22 @@ def create_app() -> FastAPI:
         lifespan=lifespan,
     )
 
+    @app.get("/api/v1/health", tags=["Global"])
+    async def health_check(redis: Redis = Depends(get_redis_client)):
+        await redis.ping()
+        return {
+            "status": "healthy",
+            "service": settings.APP_NAME,
+            "redis_connection": "ok"
+        }
+
     app.include_router(
         notification_router,
-        prefix="/api/v1",
-        tags=["Notification Service"],
+        prefix="/api/v1/notifications",
+        tags=["Notifications"],
     )
 
     return app
-
 
 app = create_app()
 
